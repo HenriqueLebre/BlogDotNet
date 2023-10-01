2193b1392e1e6764f7a1e3e78aa9e31bdfee93ec
henriqueLebre <henrique_lebre@hotmail.com>
2023-10-01 19:56:22 -0300

    Gerando ViewModel

---
+++ ViewModels/EditorCategoryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.ViewModels;
+
+public class EditorCategoryViewModel
+{
+    [Required]
+    public string Name { get; set; }
+    public string Slug { get; set; }
+
+}
+
