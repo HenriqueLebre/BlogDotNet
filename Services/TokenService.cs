af3325d021fbf31956e67770fc88ce8456d45661
henriqueLebre <henrique_lebre@hotmail.com>
2023-10-03 20:37:01 -0300

    Implementando Claims

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -1,6 +1,7 @@
 ﻿using Blog.Models;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 
 namespace Blog.Services;
@@ -13,6 +14,11 @@ public class TokenService
 
         var tokenDescriptor = new SecurityTokenDescriptor
         {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name,"henrique"),
+                new Claim(ClaimTypes.Role,"admin")
+            }),
             Expires = DateTime.UtcNow.AddDays(1),
             SigningCredentials = new SigningCredentials(
                 new SymmetricSecurityKey(key), 
