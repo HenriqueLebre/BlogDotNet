5f39c799a68a00a444bff2b0220d833f13afaa65
henriqueLebre <henrique_lebre@hotmail.com>
2023-10-04 21:35:32 -0300

    Implementando cadastro de usuario

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,6 +1,12 @@
-﻿using Blog.Services;
+﻿using Blog.Data;
+using Blog.Extensions;
+using Blog.Models;
+using Blog.Services;
+using Blog.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.ComponentModel.DataAnnotations;
 
 namespace Blog.Controllers;
 
@@ -8,12 +14,29 @@ namespace Blog.Controllers;
 public class AccountController : ControllerBase
 {
 
-    [HttpPost("v1/login")]
+    [HttpPost("v/accounts/")]
+    public async Task<IActionResult> Post(
+        [FromBody]RegisterViewModel model,
+        [FromServices]BlogDataContext context)
+    {
+        if(!ModelState.IsValid) 
+            return BadRequest(new ResultViewModel<string>(ModelState.GetErrors()));
+
+        var user = new User
+        {
+            nameof = model.Name,
+            Email = model.Email,
+            slug = model.Email.Replace("@", "-").Replace(".", "-")
+        };
+    }
+
+    [HttpPost("v1/accounts/login")]
     public IActionResult Login([FromServices]TokenService tokenService)
     {
         var token = tokenService.GenerateToken(null);
 
         return Ok(token);
     }
+
 }
 
--- Program.cs
+++ Program.cs
@@ -1,8 +1,29 @@
 using System;
+using System.Text;
+using Blog;
 using Blog.Data;
 using Blog.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
 
 builder.Services.AddControllers()
 .ConfigureApiBehaviorOptions(options =>
@@ -16,5 +37,10 @@ builder.Services.AddTransient<TokenService>();
 
 var app = builder.Build();
 
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
