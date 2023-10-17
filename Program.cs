a238b3ef99fabf90b1de7bdd561faf87b7cbe97f
henriqueLebre <henrique_lebre@hotmail.com>
2023-10-16 21:24:23 -0300

    Incluindo padronizacao nos JSON

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Text;
+using System.Text.Json.Serialization;
 using Blog;
 using Blog.Data;
 using Blog.Services;
@@ -59,6 +60,11 @@ void ConfigureMvc(WebApplicationBuilder builder)
 .ConfigureApiBehaviorOptions(options =>
 {
     options.SuppressModelStateInvalidFilter = true;
+})
+.AddJsonOptions(x =>
+{
+    x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    x.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
 });
 }
 
