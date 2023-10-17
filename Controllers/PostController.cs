ab99a5944fc56ce737f58f0941d4ae819a477cb4
henriqueLebre <henrique_lebre@hotmail.com>
2023-10-16 22:06:46 -0300

    Incluindo cache

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -1,7 +1,9 @@
 ﻿using Blog.Data;
+using Blog.ViewModels.Categories;
 using Blog.ViewModels.Posts;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Xml.Schema;
 
 namespace Blog.Controllers;
 
@@ -10,8 +12,11 @@ public class PostController : ControllerBase
 {
     [HttpGet("v1/posts")]
     public async Task<IActionResult> GetAsync(
-        [FromServices] BlogDataContext context)
+        [FromServices] BlogDataContext context,
+        [FromQuery] int page = 0,
+        [FromQuery] int pageSize = 25)
     {
+        var count = await context.Posts.AsNoTracking().CountAsync();
         var posts = await context
             .Posts
             .AsNoTracking()
@@ -26,8 +31,17 @@ public class PostController : ControllerBase
                 Category = x.Category.Name,
                 Author = $"{x.Author.Name} ({x.Author.Email})"
             })
+            .Skip(page * pageSize)
+            .Take(pageSize)
+            .OrderByDescending(x => x.LastUpdateDate)
             .ToListAsync();
-        return Ok(posts);
+        return Ok(new ResultViewModel<dynamic>(new
+            {
+                total = count,
+                page,
+                pageSize,
+                posts
+            }));
     }
 }
 
